0c6d18af3dbe6e16c0a290d38b107af45ac0a299
IShinex <x1029927373@gmail.com>
2022-11-28 22:24:41 +0800

    feat: mobile download page

--- src/components/global/CButton.vue
+++ src/components/global/CButton.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="c-button">
-		<div class="c-button-container">
+		<div class="c-button-container" flex="main:center cross:center">
 			<slot></slot>
 		</div>
 	</div>
@@ -11,35 +11,40 @@ import { computed } from 'vue'
 const props = defineProps({
 	width: {
 		type: Number,
-		default: 3
+		default: 600
 	},
 	height: {
 		type: Number,
-		default: 3
+		default: 80
 	}
 })
 
-const w = computed(() => props.width + 'em')
-const h = computed(() => props.height + 'em')
+const w = computed(() => (props.width / 19.2).toFixed(2) + 'vw')
+const h = computed(() => (props.height / 19.2).toFixed(2)  + 'vw')
 </script>
 
 <style lang="scss" scoped>
 .c-button {
 	width: v-bind(w);
+  height: v-bind(h);
 	cursor: pointer;
-	background: linear-gradient(#ffc700 78%, #f4af00 22%);
-	border: 6px solid #431e13;
-	border-radius: 20px;
+	//border: 6px solid #431e13;
+  padding: 6px;
+	border-radius: 29px;
 	box-sizing: border-box;
+  //font-size: 28px;
 	font-family: $font-family;
+  background: #431e13;
 }
 
 .c-button-container {
-	height: v-bind(h);
-	line-height: v-bind(h);
+  height: 100%;
+  line-height: 100%;
+  box-sizing: border-box;
 	border: 6px solid #ffffff;
-	border-radius: 15px;
+	border-radius: 22px;
 	text-align: center;
 	position: relative;
+  background: linear-gradient(#ffc700 82%, #f4af00 18%);
 }
 </style>
--- src/views/home/DownLoad.vue
+++ src/views/home/DownLoad.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="download-module">
     <header class="nav-bar" flex="dir:right">
-      <c-button :width="8">...</c-button>
-      <c-button :width="15">WALLET</c-button>
-      <c-button :width="15">WALLET</c-button>
+      <c-button :width="101" :height="56">···</c-button>
+      <c-button :width="231" :height="56">
+        <div class="icon-list" flex="cross:stretch box:mean">
+          <span class="icon-item"><i class="iconfont icon-twitter"></i></span>
+          <span class="line-left"></span>
+          <span class="icon-item"><i class="iconfont icon-opensea"></i></span>
+          <span class="line-right"></span>
+          <span class="icon-item"></span>
+        </div>
+      </c-button>
+      <c-button :width="231" :height="56">WALLET</c-button>
     </header>
     <main class="content">
-      <div flex="dir:left main:center cross:bottom">
+      <div class="logo-content" flex>
         <div class="logo">
           <img src="~@/assets/images/logo.png" alt="" />
         </div>
@@ -16,10 +24,10 @@
       </div>
       <p class="c-title-h1">Description: balabalabalabalabalabala</p>
       <div class="download-btn" flex="dir:left main:center">
-        <c-button :width="8" :height="2">
+        <c-button :width="250" :height="80">
           <span><i class="iconfont icon-apple"></i>IOS</span>
         </c-button>
-        <c-button :width="8" :height="2">
+        <c-button :width="250" :height="80">
           <span><i class="iconfont icon-android"></i>Android</span>
         </c-button>
       </div>
@@ -30,6 +38,7 @@
 <style lang="scss" scoped>
 .download-module {
   height: 1117px;
+  font-size: 28px;
   font-family: $font-family;
   font-weight: 700;
   background: url('~@/assets/images/part1.png') center no-repeat;
@@ -37,6 +46,36 @@
 
   .nav-bar {
     padding: 25px 38px;
+
+    .icon-list {
+      width: 100%;
+    }
+
+    .icon-item {
+      position: relative;
+
+      .iconfont {
+        font-size: 1em;
+      }
+    }
+
+    .line-left {
+      position: absolute;
+      width: 6px;
+      height: 150%;
+      top: -10px;
+      left: 33%;
+      background: #431e13;
+    }
+
+    .line-right {
+      position: absolute;
+      width: 6px;
+      height: 150%;
+      top: -10px;
+      right: 30%;
+      background: #431e13;
+    }
   }
 
   .c-button {
@@ -45,6 +84,11 @@
 
   .content {
     padding-top: 170px;
+
+    .logo-content {
+      align-items: flex-end;
+      justify-content: center;
+    }
 
     .logo {
       width: 378px;
@@ -62,12 +106,88 @@
   }
 
   .download-btn {
-    font-size: 35px;
+    font-size: .5em;
 
     .iconfont {
-      font-size: 44px;
+      font-size: 1em;
       color: #3a2016;
       margin-right: 10px;
+    }
+  }
+}
+
+@media (max-width: 483px) {
+  .download-module {
+    height: 650px;
+    background: url('~@/assets/images/mobile/part1.png') center no-repeat;
+    background-size: 100% 100%;
+
+    .nav-bar {
+      height: 109px;
+      display: block;
+      position: relative;
+
+      .c-button {
+        position: absolute;
+
+        &:first-child {
+          width: 50px;
+          height: 24px;
+          left: 40px;
+          top: 40px;
+        }
+
+        &:nth-child(2) {
+          width: 99px;
+          height: 24px;
+          top: 83px;
+          right: 40px;
+        }
+
+        &:nth-child(3) {
+          width: 99px;
+          height: 24px;
+          top: 40px;
+          right: 40px;
+        }
+      }
+
+      .line-left,.line-right {
+        height: 120%;
+      }
+    }
+
+    .content {
+      font-size: 30px;
+      .logo-content {
+        font-size: 20px;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+      }
+      .logo {
+        width: 229px;
+        height: 212px;
+      }
+      .logo-text {
+        width: 277px;
+        height: 83px;
+      }
+      .download-btn {
+        width: 277px;
+        margin: 0 auto;
+        .c-button {
+          &:first-child {
+            width: 40%;
+            height: 40px;
+          }
+          &:nth-child(2) {
+            width: 40%;
+            height: 40px;
+          }
+        }
+      }
     }
   }
 }
