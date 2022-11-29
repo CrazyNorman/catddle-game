9f5c9ea2eab97d17760df62446377fbfedc13450
IShinex <x1029927373@gmail.com>
2022-11-30 00:01:59 +0800

    feat(mobile): getYours & HowToPlay

--- src/views/home/GetYours.vue
+++ src/views/home/GetYours.vue
@@ -1,19 +1,20 @@
 <script setup>
-import { ref } from 'vue'
-import { getImageUrl } from '@/utils/index.js'
-import { Swiper, SwiperSlide } from 'swiper/vue'
-import { Autoplay, FreeMode } from 'swiper'
+import {onMounted, ref} from 'vue'
+import {getImageUrl} from '@/utils/index.js'
+import {Swiper, SwiperSlide} from 'swiper/vue'
+import {Autoplay, FreeMode} from 'swiper'
+import {isMobile} from "@/utils/index.js";
 
 const modules = ref([Autoplay, FreeMode])
 
-const urls = ref([
-  'get-yours/cats1.png',
-  'get-yours/cats2.png',
-  'get-yours/cats3.png',
-  'get-yours/cats4.png',
-  'get-yours/cats5.png',
-])
-
+const urls = ref([])
+function getUrls() {
+  const baseUrl = isMobile() ? 'get-yours/mobile/' : 'get-yours/'
+  for (let i = 1; i< 6; i++) {
+    urls.value.push(getImageUrl(`${baseUrl}cats${i}.png`))
+  }
+}
+onMounted(() => getUrls())
 </script>
 
 <template>
@@ -21,15 +22,16 @@ const urls = ref([
     <div class="c-title-h1">Description: balabalabalabalabalabala</div>
     <div class="c-title-h2">Description: balabalabalabalabalabala</div>
     <swiper
-        :loop="true"
-        :speed="3000"
-        :freeMode="true"
-        :autoplay="{delay: 1000, disableOnInteraction: false}"
-        :slidesPerView="3"
-        :spaceBetween="2"
-        :modules="modules">
+      :loop="true"
+      :speed="3000"
+      :freeMode="true"
+      :autoplay="{delay: 1000, disableOnInteraction: false}"
+      :slidesPerView="isMobile() ? 1 : 3"
+      :spaceBetween="2"
+      :modules="modules"
+    >
       <swiper-slide v-for="url in urls">
-        <img :src="getImageUrl(url)" alt="" />
+        <img :src="url" alt="" />
       </swiper-slide>
     </swiper>
     <c-button> Get Your catddle</c-button>
@@ -62,6 +64,24 @@ const urls = ref([
   .swiper-slide img {
     width: auto;
     height: 100%;
+  }
+}
+
+@media (max-width: 483px) {
+  .get-yours {
+    height: 650px;
+    background: url('~@/assets/images/mobile/part3.png') center no-repeat;
+    background-size: 100% 100%;
+
+    .swiper {
+      width: 100%;
+      height: 356px;
+    }
+    .c-button {
+      width: 228px;
+      height: 40px;
+      font-size: 18px;
+    }
   }
 }
 </style>
--- src/views/home/HowToPlay.vue
+++ src/views/home/HowToPlay.vue
@@ -2,22 +2,40 @@
 	<div class="how-play">
 		<div class="c-title-h1">Description: balabalabalabalabalabala</div>
 		<div class="c-title-h2">Description: balabalabalabalabalabala</div>
-		<swiper
-			:loop="true"
-			:speed="3000"
-			:freeMode="true"
-			:autoplay="{
+		<swiper v-show="false"></swiper>
+    <swiper
+        :loop="true"
+        :speed="3000"
+        :freeMode="true"
+        :autoplay="{
 				delay: 1000,
 				disableOnInteraction: false
 			}"
-			:slidesPerView="6"
-			:spaceBetween="2"
-			:modules="modules"
-		>
-			<swiper-slide v-for="i in 8">
-				<img src="~@/assets/images/game1.png" alt="" />
-			</swiper-slide>
-		</swiper>
+        :slidesPerView="isMobile() ? 4 : 6"
+        :spaceBetween="2"
+        :modules="modules"
+    >
+      <swiper-slide v-for="img in images">
+        <img :src="img" alt="" />
+      </swiper-slide>
+    </swiper>
+    <swiper
+        v-if="isMobile()"
+        :loop="true"
+        :speed="3000"
+        :freeMode="true"
+        :autoplay="{
+				delay: 1000,
+				disableOnInteraction: false
+			}"
+        :slidesPerView="4"
+        :spaceBetween="2"
+        :modules="modules"
+    >
+      <swiper-slide v-for="img in images">
+        <img :src="img" alt="" />
+      </swiper-slide>
+    </swiper>
 		<c-button> How to play </c-button>
 	</div>
 </template>
@@ -25,51 +43,66 @@
 <script setup>
 import { Swiper, SwiperSlide } from 'swiper/vue'
 import { Autoplay, FreeMode } from 'swiper'
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import { getImageUrl } from "@/utils"
+import { isMobile } from "@/utils";
 
 const modules = ref([Autoplay, FreeMode])
+const images = ref([])
+
+function getImages () {
+  for (let i = 1; i < 6; i++) {
+    images.value.push(getImageUrl(`how-to-play/play-${i}.png`))
+  }
+}
+
+onMounted(() => getImages())
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .how-play {
-	height: 1038px;
-	padding: 90px 0;
-	font-family: 'Chalkboard';
-	font-weight: 700;
-	background: url('~@/assets/images/part2.png') no-repeat;
-	background-size: cover;
+  height: 1038px;
+  padding: 90px 0;
+  font-family: 'Chalkboard';
+  font-weight: 700;
+  background: url('~@/assets/images/part2.png') no-repeat;
+  background-size: cover;
+
+  .swiper {
+    width: 100%;
+    height: 456px;
+    margin: 45px 0 69px;
+  }
+
+  .swiper-slide img {
+    width: auto;
+    height: 100%;
+  }
+
+  .c-button {
+    font-size: 35px;
+    margin: 0 auto;
+  }
 }
 
-.description {
-	font-style: normal;
-	font-weight: 700;
-	font-size: 50px;
-	line-height: 64px;
-	text-align: center;
-	color: #ffffff;
-}
+@media (max-width: 483px) {
+  .how-play {
+    height: 650px;
+    background: url('~@/assets/images/mobile/part2.png') center no-repeat;
+    background-size: 100% 100%;
 
-.description-second {
-	font-size: 25px;
-	text-align: center;
-	color: #ffffff;
-}
-.swiper {
-	width: 100%;
-	height: 456px;
-	margin: 45px 0 69px;
-}
+    .swiper {
+      width: 100%;
+      height: 150px;
+      margin-top: 30px;
+    }
 
-.swiper-slide img {
-	width: auto;
-	height: 100%;
+    .c-button {
+      width: 228px;
+      height: 40px;
+      font-size: 18px;
+      margin-top: 70px;
+    }
+  }
 }
-.c-button {
-	font-size: 35px;
-	margin: 0 auto;
-}
-
-/*.swiper :deep(.swiper-wrapper) {*/
-/*	transition-timing-function: linear;*/
-/*}*/
 </style>
